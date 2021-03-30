4d8ec62e8edaeb6cfe18f961a6c40a5bad2b4db4
sergevas <brighterr@gmail.com>
2021-03-31 01:39:08 +0300

    In progress

--- ch02/rest-api-demo/src/test/java/com/apress/messaging/RestApiDemoApplicationTests.java
+++ ch02/rest-api-demo/src/test/java/com/apress/messaging/RestApiDemoApplicationTests.java
@@ -1,7 +1,7 @@
 package com.apress.messaging;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 public class RestApiDemoApplicationTests {
